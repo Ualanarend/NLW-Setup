1209db74c4cbd8b7b383b1e8fb5eef4e28fa8e62
Ueslei <ualanarend49@gmail.com>
2023-01-18 20:38:41 -0300

    aula 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["02-01", "02-03", "02-05"],
+  takePills: ["02-02", "02-04", "02-06"],
+  food: ["02-08", "02-10", "02-12"],
+  water: ["02-09", "02-11", "02-12"],
+  study: ["02-07"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
