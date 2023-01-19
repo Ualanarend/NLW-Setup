d19115d898def165b6522a4bdf492c61b8b469c6
Ueslei <ualanarend49@gmail.com>
2023-01-19 19:20:11 -0300

    troll

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
+button.addEventListener('click', add)
+
+function add() {
+  alert('QUEM CLICOU É VIADO')
+}
+
+const data = { 
   run: ["02-01", "02-03", "02-05"],
   takePills: ["02-02", "02-04", "02-06"],
   food: ["02-08", "02-10", "02-12"],
